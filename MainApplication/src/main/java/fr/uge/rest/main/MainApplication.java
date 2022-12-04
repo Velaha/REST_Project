d3fe78d70d32faac1f017ab259d6c43fa86348c3
FxTsuna <FxTsuna@outlook.fr>
2022-12-05 00:03:21 +0100

    For testing

--- MainApplication/src/main/java/fr/uge/rest/main/MainApplication.java
+++ MainApplication/src/main/java/fr/uge/rest/main/MainApplication.java
@@ -3,9 +3,22 @@ package fr.uge.rest.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class MainApplication {
 	public static void main(String[] args) {
+		/*
+		if (args.length == 0) {
+			System.out.println(("Please give one argument"));
+			return;
+		}
+		var port = args[0];
+		SpringApplication app = new SpringApplication(MainApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
+
+		 */
 		SpringApplication.run(MainApplication.class, args);
 	}
 }
