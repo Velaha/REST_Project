d04a35221e50c0af77611a59cc7c8dacb7c06426
FxTsuna <FxTsuna@outlook.fr>
2022-12-04 17:24:45 +0100

    Repaired_spring trying to connect with service web

--- MainApplication/src/main/java/fr/uge/rest/webservice/shop/ShopClient.java
+++ MainApplication/src/main/java/fr/uge/rest/webservice/shop/ShopClient.java
@@ -1,8 +1,17 @@
-/*package fr.uge.rest.webservice.shop;
+package fr.uge.rest.webservice.shop;
 
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
-public class ShopClient extends WebServiceGatewaySupport {
+import fr.uge.rest.serviceweb.shop.Shop;
+import fr.uge.rest.serviceweb.shop.ShopService;
+import fr.uge.rest.serviceweb.shop.ShopServiceLocator;
+import org.springframework.context.annotation.Bean;
 
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+
+public class ShopClient  {
+    @Bean
+    public Shop getShop() throws ServiceException {
+        return new ShopServiceLocator().getShop();
+    }
 }
-*/
